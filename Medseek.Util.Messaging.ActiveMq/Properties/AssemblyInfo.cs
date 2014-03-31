2d055e3e132b12b425d89dbfb3ec6f562e32c6f3
Fred Westrom <fred.westrom@medseek.com>
2014-03-31 14:03:50 -0500

    Removed the poc-level code for ActiveMQ messaging library plugin implementation, since it has fallen out of sync with RabbitMQ messaging and is not going to be maintained due to lack of demand in business goals.

--- Medseek.Util.Messaging.ActiveMq/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-using Medseek.Util.Properties;
-
-[assembly: AssemblyDescription(UtilAssembly.Description + " (ActiveMQ plugin)")]
-[assembly: AssemblyTitle(UtilAssembly.Title + ".Messaging.ActiveMq")]
-[assembly: Guid("f9b4f3b2-b153-4dd8-b528-cc0379b30ce2")]
