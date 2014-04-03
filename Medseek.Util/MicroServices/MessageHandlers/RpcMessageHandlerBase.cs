367ac50e21ffffc72a584a06486d0846be4777e7
David Martines <david.martines@medseek.com>
2014-04-03 12:04:49 -0500

    Adding renamed files missing from previous commit.

---
+++ Medseek.Util/MicroServices/MessageHandlers/RpcMessageHandlerBase.cs
@@ -0,0 +1,29 @@
+﻿namespace Medseek.Util.MicroServices.MessageHandlers
+{
+	/// <summary>
+	/// Generic base class for two-way message handlers.
+	/// </summary>
+	/// <typeparam name="TMessage"></typeparam>
+	/// <typeparam name="TReply"></typeparam>
+	public abstract class RpcMessageHandlerBase<TMessage, TReply> : IRpcMessageHandler
+		where TMessage : MessageBase
+		where TReply : ReplyBase
+	{
+		/// <summary>
+		/// Handles the message.
+		/// </summary>
+		/// <param name="message"></param>
+		/// <returns></returns>
+		public ReplyBase Handle(MessageBase message)
+		{
+			return Handle(message as TMessage);
+		}
+
+		/// <summary>
+		/// Handles the typed message.
+		/// </summary>
+		/// <param name="message"></param>
+		/// <returns></returns>
+		public abstract TReply Handle(TMessage message);
+	}
+}
