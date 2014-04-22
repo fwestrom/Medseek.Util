f533b573b36290b6ceaedf4305bd3fe62f745959
Fred Westrom <fred.westrom@medseek.com>
2014-04-22 13:50:16 -0500

    Pulled MicroServiceLookup.Resolve into IMicroServiceLookup.

--- Medseek.Util/MicroServices/Lookup/IMicroServiceLookup.cs
+++ Medseek.Util/MicroServices/Lookup/IMicroServiceLookup.cs
@@ -1,5 +1,8 @@
 ﻿namespace Medseek.Util.MicroServices.Lookup
 {
+    using System;
+    using Medseek.Util.Messaging;
+
     /// <summary>
     /// Interface for types that provide micro-service lookup functionality 
     /// for translating addressing information at runtime to the actual values 
@@ -7,5 +10,22 @@
     /// </summary>
     public interface IMicroServiceLookup
     {
+        /// <summary>
+        /// Resolves a micro-service using the lookup functionality and the 
+        /// specified original address.
+        /// </summary>
+        /// <param name="address">
+        /// The original address corresponding to the event, request, service, 
+        /// or other relevant messaging primitive. 
+        /// </param>
+        /// <param name="timeout">
+        /// The maximum amount of time to wait for a query to complete before 
+        /// it will be aborted.
+        /// </param>
+        /// <returns>
+        /// The resolved address, or null if no reply was returned by the 
+        /// query.
+        /// </returns>
+        MqAddress Resolve(MqAddress address, TimeSpan timeout);
     }
 }
---
+++ Medseek.Util/MicroServices/MicroServiceSerializerExtensions.cs
@@ -0,0 +1,55 @@
+﻿namespace Medseek.Util.MicroServices
+{
+    using System.IO;
+
+    /// <summary>
+    /// Provides extension methods for working with the micro-service serializer.
+    /// </summary>
+    public static class MicroServiceSerializerExtensions
+    {
+        /// <summary>
+        /// Deserializes an object from a specified content type and source.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The type of the object to read.
+        /// </typeparam>
+        /// <param name="serializer">
+        /// The serializer to use for reading the object.
+        /// </param>
+        /// <param name="contentType">
+        /// The content type to deserialize.
+        /// </param>
+        /// <param name="source">
+        /// The source of the serialized object data.
+        /// </param>
+        public static T Deserialize<T>(this IMicroServiceSerializer serializer, string contentType, Stream source)
+        {
+            var values = serializer.Deserialize(contentType, new[] { typeof(T) }, source);
+            var value = (T)values[0];
+            return value;
+        }
+
+        /// <summary>
+        /// Serializes an object to a specified content type and destination.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The type of the object to serialize.
+        /// </typeparam>
+        /// <param name="serializer">
+        /// The serializer to use for writing the object.
+        /// </param>
+        /// <param name="contentType">
+        /// The content type to use for serialization.
+        /// </param>
+        /// <param name="destination">
+        /// The destination of the serialized object data.
+        /// </param>
+        /// <param name="value">
+        /// The value to serialize.
+        /// </param>
+        public static void Serialize<T>(this IMicroServiceSerializer serializer, string contentType, Stream destination, T value)
+        {
+            serializer.Serialize(contentType, typeof(T), value, destination);
+        }
+    }
+}
